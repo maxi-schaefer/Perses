ba05579c8c6b05633c4c84b6d91d8255a7d6fb0d
max <95922236+gokiimax@users.noreply.github.com>
2022-06-07 19:59:55 +0200

    Added libs folder and updated README.md

--- README.md
+++ README.md
@@ -2,29 +2,28 @@
 ## Original CMD Print:
 ![](https://raw.githubusercontent.com/gokiimax/Perses/main/images/cmd_demo.png)
 
-## [Follow me on Twitter](https://twitter.com/gokimax_x)
-## [Join the Discord Server](https://discord.com)
+##[Follow me on Twitter](https://twitter.com/gokimax_x)
+##[Join the Discord Server](https://discord.com)
 
 
 **Requirements:**
 - Windows 10/11
 - Python
 - PIP Installations
-
 <br>
-
+<br>
 - [x] - Updated Frequently
 - [x] - Dev / Coded by maxシ#6858
 - [ ] -  Created Community Server
 
-# ❗- Installation:
+#❗- Installation:
 ```
 git clone https://github.com/gokiimax/Perses.git
 cd Perses
 
 python3 -m pip install -r requirements.txt
 
-cd src/Server
+cd Server
 python3 perses.py
 ```
 
---
+++ src/Server/lib/utils.py
@@ -0,0 +1,75 @@
+from lib.terminal import rgb, custom_banner, clear_custom, set_console_title
+import socket
+import requests
+import webbrowser
+from threading import Thread
+from time import sleep
+from itertools import cycle
+from shutil import get_terminal_size
+
+#=====================================================================#
+
+def ip_lookup():
+    enter_ip = input(f"{rgb(105, 67, 171, '~# ')}IP Address: ")
+    response = requests.get(f"https://geolocation-db.com/json/{enter_ip}&position=true").json()
+    print("\n")  
+    for key, value in response.items():
+        print(f"\t{rgb(105, 67, 171, key)}{rgb(255, 255, 255, ': ')}{rgb(105, 67, 171, value)}")
+    print("\n")    
+
+#=====================================================================#
+
+def domain_name():
+    try:
+        print(socket.gethostbyname(input(f"{rgb(105, 67, 171, '~# ')}Domain name: ")))
+    except:
+        return
+
+#=====================================================================#
+
+def open_link(url):
+    webbrowser.open(url, new=0, autoraise=True)
+
+#=====================================================================#
+
+class Loader:
+    def __init__(self, desc="Loading", end="Done!", timeout=0.1):
+        self.desc = desc
+        self.end = end
+        self.timeout = timeout
+
+        self._thread = Thread(target=self._animate, daemon=True)
+        self.steps = ["⢿", "⣻", "⣽", "⣾", "⣷", "⣯", "⣟", "⡿"] # "⢿", "⣻", "⣽", "⣾", "⣷", "⣯", "⣟", "⡿"
+        self.done = False
+
+    def start(self):
+        self._thread.start()
+        return self
+
+    def _animate(self):
+        for c in cycle(self.steps):
+            if self.done:
+                break
+            print(rgb(105, 67, 171, f"\r{self.desc} {c}"), flush=True, end="")
+            sleep(self.timeout)
+
+    def __enter__(self):
+        self.start()
+
+    def stop(self):
+        self.done = True
+        cols = get_terminal_size((80, 20)).columns
+        print("\r" + " " * cols, end="", flush=True)
+        print(f"\r{self.end}", flush=True)
+
+#=====================================================================#
+
+def load(loadingText:str):
+    clear_custom()
+    custom_banner("Perses")
+    set_console_title(f"Perses | {loadingText}")
+    loader = Loader(loadingText).start()
+    for i in range(10):
+        sleep(0.25)
+    set_console_title(f"Perses | Done!")
+    loader.stop()
